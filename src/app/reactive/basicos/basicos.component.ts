d9fb85ce71a2c0048cc95b3cc2cc897f82dfa89b
LAPTOP-PJ1275UD\Matias <matias.angeluk@gmail.com>
2022-05-22 00:16:25 -0300

    formulario reactivo basico con validacion

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -15,14 +15,33 @@ export class BasicosComponent implements OnInit {
   //   existencias: new FormControl(0)
   // })
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.minLength(3)] ],
-    precio: [0, [Validators.required, Validators.min(0)]],
-    existencias: [0, [Validators.required, Validators.min(0)]]
+    nombre: [ , [Validators.required, Validators.minLength(3)] ],
+    precio: [ , [Validators.required, Validators.min(0)]],
+    existencias: [ , [Validators.required, Validators.min(0)]]
   })
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: '',
+      precio: 0,
+      existencias: 0
+    })
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched()
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
+
+  campoValido(campo: string) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
   }
 
 }
